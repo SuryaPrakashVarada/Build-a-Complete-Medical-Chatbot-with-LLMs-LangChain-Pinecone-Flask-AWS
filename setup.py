c5e5023cd0f0b744edcc31d188678d567fded1c7
Surya <surya.mouni2011@gmail.com>
2026-05-19 19:46:50 +0530

    Initial commit of new project

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+<<<<<<< HEAD
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Boktiar Ahmed Bappy",
+    author_email="entbappy73@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+=======
+    name="medical-chat-bot",
+    version="0.1.0",
+    author="Surya Prakash Varada",
+    author_email="suryaprakash.varada103@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+>>>>>>> 8bc5d313d5b756279f2f196be628d2da6e129a55
---
+++ template.sh
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+# Creating directory 
+mkdir -p src
+mkdir -p research 
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py 
+touch src/prompt.py
+touch .env 
+touch setup.py
+touch app.py
+touch research/trials.ipynb 
+touch requirements.txt
+
+
+echo "Directory and files created successfully!."
+=======
+mkdir -p src
+mkdir -p research
+
+touch src/ __init__.py
+touch src/helper.py
+touch src/propmt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+
+echo "Directory and files created successfully"
+>>>>>>> 8bc5d313d5b756279f2f196be628d2da6e129a55
